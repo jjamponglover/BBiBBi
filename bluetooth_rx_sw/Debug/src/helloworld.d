60ef2a742014a2fb22b97a3c414d8de9a03717f3
Sangsu Lee <si464640@gmail.com>
2024-10-23 02:08:39 +0900

    upload vitis source

---
+++ bluetooth_rx_sw/Debug/src/helloworld.d
@@ -0,0 +1,51 @@
+src/helloworld.o src/helloworld.o: ../src/helloworld.c ../src/platform.h \
+ ../src/platform_config.h \
+ D:/LSS/_workspace/vitis/bluetooth_pj_hw/export/bluetooth_pj_hw/sw/bluetooth_pj_hw/standalone_domain/bspinclude/include/xil_printf.h \
+ D:/LSS/_workspace/vitis/bluetooth_pj_hw/export/bluetooth_pj_hw/sw/bluetooth_pj_hw/standalone_domain/bspinclude/include/xil_types.h \
+ D:/LSS/_workspace/vitis/bluetooth_pj_hw/export/bluetooth_pj_hw/sw/bluetooth_pj_hw/standalone_domain/bspinclude/include/xparameters.h \
+ D:/LSS/_workspace/vitis/bluetooth_pj_hw/export/bluetooth_pj_hw/sw/bluetooth_pj_hw/standalone_domain/bspinclude/include/bspconfig.h \
+ D:/LSS/_workspace/vitis/bluetooth_pj_hw/export/bluetooth_pj_hw/sw/bluetooth_pj_hw/standalone_domain/bspinclude/include/xparameters.h \
+ D:/LSS/_workspace/vitis/bluetooth_pj_hw/export/bluetooth_pj_hw/sw/bluetooth_pj_hw/standalone_domain/bspinclude/include/xil_exception.h \
+ D:/LSS/_workspace/vitis/bluetooth_pj_hw/export/bluetooth_pj_hw/sw/bluetooth_pj_hw/standalone_domain/bspinclude/include/xiic.h \
+ D:/LSS/_workspace/vitis/bluetooth_pj_hw/export/bluetooth_pj_hw/sw/bluetooth_pj_hw/standalone_domain/bspinclude/include/xil_assert.h \
+ D:/LSS/_workspace/vitis/bluetooth_pj_hw/export/bluetooth_pj_hw/sw/bluetooth_pj_hw/standalone_domain/bspinclude/include/xstatus.h \
+ D:/LSS/_workspace/vitis/bluetooth_pj_hw/export/bluetooth_pj_hw/sw/bluetooth_pj_hw/standalone_domain/bspinclude/include/xiic_l.h \
+ D:/LSS/_workspace/vitis/bluetooth_pj_hw/export/bluetooth_pj_hw/sw/bluetooth_pj_hw/standalone_domain/bspinclude/include/xil_io.h \
+ D:/LSS/_workspace/vitis/bluetooth_pj_hw/export/bluetooth_pj_hw/sw/bluetooth_pj_hw/standalone_domain/bspinclude/include/xil_printf.h \
+ D:/LSS/_workspace/vitis/bluetooth_pj_hw/export/bluetooth_pj_hw/sw/bluetooth_pj_hw/standalone_domain/bspinclude/include/mb_interface.h \
+ D:/LSS/_workspace/vitis/bluetooth_pj_hw/export/bluetooth_pj_hw/sw/bluetooth_pj_hw/standalone_domain/bspinclude/include/xil_exception.h \
+ D:/LSS/_workspace/vitis/bluetooth_pj_hw/export/bluetooth_pj_hw/sw/bluetooth_pj_hw/standalone_domain/bspinclude/include/xuartlite.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+D:/LSS/_workspace/vitis/bluetooth_pj_hw/export/bluetooth_pj_hw/sw/bluetooth_pj_hw/standalone_domain/bspinclude/include/xil_printf.h:
+
+D:/LSS/_workspace/vitis/bluetooth_pj_hw/export/bluetooth_pj_hw/sw/bluetooth_pj_hw/standalone_domain/bspinclude/include/xil_types.h:
+
+D:/LSS/_workspace/vitis/bluetooth_pj_hw/export/bluetooth_pj_hw/sw/bluetooth_pj_hw/standalone_domain/bspinclude/include/xparameters.h:
+
+D:/LSS/_workspace/vitis/bluetooth_pj_hw/export/bluetooth_pj_hw/sw/bluetooth_pj_hw/standalone_domain/bspinclude/include/bspconfig.h:
+
+D:/LSS/_workspace/vitis/bluetooth_pj_hw/export/bluetooth_pj_hw/sw/bluetooth_pj_hw/standalone_domain/bspinclude/include/xparameters.h:
+
+D:/LSS/_workspace/vitis/bluetooth_pj_hw/export/bluetooth_pj_hw/sw/bluetooth_pj_hw/standalone_domain/bspinclude/include/xil_exception.h:
+
+D:/LSS/_workspace/vitis/bluetooth_pj_hw/export/bluetooth_pj_hw/sw/bluetooth_pj_hw/standalone_domain/bspinclude/include/xiic.h:
+
+D:/LSS/_workspace/vitis/bluetooth_pj_hw/export/bluetooth_pj_hw/sw/bluetooth_pj_hw/standalone_domain/bspinclude/include/xil_assert.h:
+
+D:/LSS/_workspace/vitis/bluetooth_pj_hw/export/bluetooth_pj_hw/sw/bluetooth_pj_hw/standalone_domain/bspinclude/include/xstatus.h:
+
+D:/LSS/_workspace/vitis/bluetooth_pj_hw/export/bluetooth_pj_hw/sw/bluetooth_pj_hw/standalone_domain/bspinclude/include/xiic_l.h:
+
+D:/LSS/_workspace/vitis/bluetooth_pj_hw/export/bluetooth_pj_hw/sw/bluetooth_pj_hw/standalone_domain/bspinclude/include/xil_io.h:
+
+D:/LSS/_workspace/vitis/bluetooth_pj_hw/export/bluetooth_pj_hw/sw/bluetooth_pj_hw/standalone_domain/bspinclude/include/xil_printf.h:
+
+D:/LSS/_workspace/vitis/bluetooth_pj_hw/export/bluetooth_pj_hw/sw/bluetooth_pj_hw/standalone_domain/bspinclude/include/mb_interface.h:
+
+D:/LSS/_workspace/vitis/bluetooth_pj_hw/export/bluetooth_pj_hw/sw/bluetooth_pj_hw/standalone_domain/bspinclude/include/xil_exception.h:
+
+D:/LSS/_workspace/vitis/bluetooth_pj_hw/export/bluetooth_pj_hw/sw/bluetooth_pj_hw/standalone_domain/bspinclude/include/xuartlite.h:
---
+++ bluetooth_rx_sw/Debug/src/platform.d
@@ -0,0 +1,25 @@
+src/platform.o src/platform.o: ../src/platform.c \
+ D:/LSS/_workspace/vitis/bluetooth_pj_hw/export/bluetooth_pj_hw/sw/bluetooth_pj_hw/standalone_domain/bspinclude/include/xparameters.h \
+ D:/LSS/_workspace/vitis/bluetooth_pj_hw/export/bluetooth_pj_hw/sw/bluetooth_pj_hw/standalone_domain/bspinclude/include/xil_cache.h \
+ D:/LSS/_workspace/vitis/bluetooth_pj_hw/export/bluetooth_pj_hw/sw/bluetooth_pj_hw/standalone_domain/bspinclude/include/mb_interface.h \
+ D:/LSS/_workspace/vitis/bluetooth_pj_hw/export/bluetooth_pj_hw/sw/bluetooth_pj_hw/standalone_domain/bspinclude/include/xil_types.h \
+ D:/LSS/_workspace/vitis/bluetooth_pj_hw/export/bluetooth_pj_hw/sw/bluetooth_pj_hw/standalone_domain/bspinclude/include/xil_assert.h \
+ D:/LSS/_workspace/vitis/bluetooth_pj_hw/export/bluetooth_pj_hw/sw/bluetooth_pj_hw/standalone_domain/bspinclude/include/xil_exception.h \
+ D:/LSS/_workspace/vitis/bluetooth_pj_hw/export/bluetooth_pj_hw/sw/bluetooth_pj_hw/standalone_domain/bspinclude/include/xparameters.h \
+ ../src/platform_config.h
+
+D:/LSS/_workspace/vitis/bluetooth_pj_hw/export/bluetooth_pj_hw/sw/bluetooth_pj_hw/standalone_domain/bspinclude/include/xparameters.h:
+
+D:/LSS/_workspace/vitis/bluetooth_pj_hw/export/bluetooth_pj_hw/sw/bluetooth_pj_hw/standalone_domain/bspinclude/include/xil_cache.h:
+
+D:/LSS/_workspace/vitis/bluetooth_pj_hw/export/bluetooth_pj_hw/sw/bluetooth_pj_hw/standalone_domain/bspinclude/include/mb_interface.h:
+
+D:/LSS/_workspace/vitis/bluetooth_pj_hw/export/bluetooth_pj_hw/sw/bluetooth_pj_hw/standalone_domain/bspinclude/include/xil_types.h:
+
+D:/LSS/_workspace/vitis/bluetooth_pj_hw/export/bluetooth_pj_hw/sw/bluetooth_pj_hw/standalone_domain/bspinclude/include/xil_assert.h:
+
+D:/LSS/_workspace/vitis/bluetooth_pj_hw/export/bluetooth_pj_hw/sw/bluetooth_pj_hw/standalone_domain/bspinclude/include/xil_exception.h:
+
+D:/LSS/_workspace/vitis/bluetooth_pj_hw/export/bluetooth_pj_hw/sw/bluetooth_pj_hw/standalone_domain/bspinclude/include/xparameters.h:
+
+../src/platform_config.h:
